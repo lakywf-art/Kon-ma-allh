94885d7b08028b27959d8c53078e88cc07eb80bb
Anonymous <lakywf@gmail.com>
2026-09-05 20:51:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = 'kan-ma-allah-v1';
+const CACHE_NAME = 'kan-ma-allah-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png.jpg',
-  './icon-512.png.jpg'
+  './icon-192.jpg',
+  './icon-512.jpg'
 ];
 
-// تثبيت الملفات في الذاكرة للعمل بدون إنترنت
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-// استدعاء الملفات المخزنة عند قطع الإنترنت
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
+                      

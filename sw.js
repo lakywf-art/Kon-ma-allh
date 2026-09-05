2dc1e426e203a419cc33190c50c321851bb0b8fd
Anonymous <lakywf@gmail.com>
2026-09-05 21:44:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,15 @@
-const CACHE_NAME = 'kan-ma-allah-v2';
-const ASSETS = [
-  '/Kon-ma-allh/',
-  '/Kon-ma-allh/index.html',
-  '/Kon-ma-allh/manifest.json',
-  '/Kon-ma-allh/icon-192.jpg',
-  '/Kon-ma-allh/icon-512.jpg'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  return self.clients.claim();
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
-    
